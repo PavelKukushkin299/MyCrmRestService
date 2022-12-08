a44e33a89802b574f2f74b3e7befd2ab3805a84a
Малоземов Стас <smalozemov@smart-ts.ru>
2022-12-08 12:58:19 +0500

    Создание библиотеки

---
+++ Code/STS.CrmConnector/STS.CrmConnector/Core/DynamicsCrmConnector.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace STS.CrmConnector.Core
+{
+    public class DynamicsCrmConnector : IConnector
+    {
+        public void Execute()
+        {
+            Console.WriteLine("Начало");
+            Console.WriteLine("Конец");
+        }
+    }
+}
